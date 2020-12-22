ec3229a45f3fd3091b83c1e1452c84db8862dc44
Alex Dan <lxlxdan@protonmail.com>
2020-12-22 21:33:11 +0300

    Deploy Test

--- Sources/LxlxdanGithubIo/main.swift
+++ Sources/LxlxdanGithubIo/main.swift
@@ -21,6 +21,7 @@ struct LxlxdanGithubIo: Website {
     var imagePath: Path? { nil }
 }
 
-try LxlxdanGithubIo().publish(using: [
-    .deploy(using: .gitHub("lxlxdan/BTCRUCLUB", useSSH: false))
-])
+try LxlxdanGithubIo().publish(
+      withTheme: .foundation,
+     deployedUsing: .gitHub("lxlxdan/BTCRUCLUB")
+ )
